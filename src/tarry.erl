b45bd64d0c7b63e69cdc4a1ea427757b690619ce
Joe Groocock <frebib@gmail.com>
2018-03-13 18:42:06 +0000

    Add simple line-by-line echo'ing

--- src/tarry.erl
+++ src/tarry.erl
@@ -2,4 +2,11 @@
 -export([main/0]).
 
 main() ->
-    io:fwrite("Hello, World!\n").
+  case io:get_line("") of
+    eof  -> ok;
+    Line ->
+      % This leaks memory so don't run it with a lot of data
+      % tail recursive my arse... 🙄
+      io:fwrite(Line),
+      main()
+  end.
