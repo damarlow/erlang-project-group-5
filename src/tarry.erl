dd65945e127e9d94b33960746755793acfff9656
Dave Marlow <dam486@cs.bham.ac.uk>
2018-03-17 18:49:40 +0000

    Added a function to spawn processes for each node. Not yet anything to deal with linking them together

--- src/tarry.erl
+++ src/tarry.erl
@@ -1,6 +1,6 @@
 -module(tarry).
 
--export([main/0]).
+-export([main/0, testSpawned/2]).
 
 readInput(Lines) ->
   case io:get_line("") of
@@ -14,5 +14,26 @@ readInput(Lines) ->
   end.
 
 main() ->
-  Out = readInput([]),
-  io:format("~p~n", [Out]).
+    Out = readInput([]),
+    spawnNodes(Out).
+%    io:format("~p~n", [Out]).
+
+spawnNodes([Head | Tail])->
+    % Head will be the initiator node, tail is the list to build graph from
+    % Use a list composition to get the name of each node (i.e. first elem of each sublist)
+    NodeNames = [lists:sublist(Node, 1, 1) || Node <- Tail],
+    % The neighbours of each will be the rest of the elements of each sublist
+    %Neighbours = 
+    [dropFirst(SubList) || SubList <- Tail],
+    % Pids = 
+    [spawn(tarry, testSpawned, [Name, Name =:= Head]) || Name <- NodeNames].
+
+
+% Confirm that a node is spawned correctly
+testSpawned(Node, Flag)->
+    io:format("Node ~p with id ~p, initiator: ~p ~n", [self(), Node, Flag ]).
+
+
+% To make it look nicer when dropping the first and keeping in order
+dropFirst(List)->
+    lists:reverse(lists:droplast(lists:reverse(List))).
