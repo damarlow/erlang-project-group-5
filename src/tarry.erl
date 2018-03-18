79ae9a40ac370d0fee29fbdc32c78002bf4e0296
Joe Groocock <frebib@gmail.com>
2018-03-18 11:56:12 +0000

    Notify each node of it's own neighbours.
    Depending on how we choose to approach this, a Node need not know whether it is the initiator; instead every Node should wait for a message to be passed to it, this way we can choose the initiator by sending the first message from the spawning thread.

--- src/tarry.erl
+++ src/tarry.erl
@@ -1,6 +1,6 @@
 -module(tarry).
 
--export([main/0, handleSpawned/2, getNeighbours/2]).
+-export([main/0, handleSpawned/1, getNeighbours/2]).
 
 readInput(Lines) ->
   case io:get_line("") of
@@ -19,29 +19,32 @@ main() ->
     spawnNodes(Out).
     %    io:format("~p~n", [Out]).
 
-spawnNodes([Head | Tail])->
-    % Head will be the initiator node, tail is the list to build graph from
+spawnNodes([Initiator | Tail]) ->
+    % Tail is the list to build graph from, Initiator is the node to start Tarry with
     % Use a list composition to get the name of each node (i.e. first elem of each sublist)
     NodeNames = [lists:sublist(Node, 1, 1) || Node <- Tail],
     % The neighbours of each are the rest of the elements of each sublist
     Neighbours = [lists:reverse(lists:droplast(lists:reverse(SubList)))|| SubList <- Tail],
     % This spawns nodes and keeps Pids
-    Pids = [spawn(tarry, handleSpawned, [Name, Name =:= Head]) || Name <- NodeNames],
+    Pids = [spawn(tarry, handleSpawned, [Name]) || Name <- NodeNames],
     % Zip the Pids together with corresponding nodes
     NodeIDs = lists:zip(NodeNames, Pids),
     % Get a list of {PID, Neighbour-PID-list} tuples, giving a list of nodes that are neighbours for each node
     NeighbourIDs = [getNeighbours(N, NodeIDs)|| N <- lists:zip(NodeIDs, Neighbours)],
     % Still need to do something with this
-    % Possibly [Pid ! Neighbours || {Pid, Neighbours} <- NeighbourIDs]
+    [Pid ! NeighbourPids || {Pid, NeighbourPids} <- NeighbourIDs],
     % Need to work out how to handle that though
-    io:format("~p~n", [NeighbourIDs]).
+    io:format("Initiating Tarry with ~p~n", [Initiator]).
 
-getNeighbours({{_, PID}, NeighboursForOne}, NodeIDs)->
+getNeighbours({{_, PID}, NeighboursForOne}, NodeIDs) ->
     % Function to get the PID for a node
     FuncMap = fun(Node) -> element(2, lists:keyfind([Node], 1, NodeIDs)) end,
     % Return a tuple of the PID and the PIDs of its neighbours
     {PID, lists:map(FuncMap, NeighboursForOne)}.
-		
-% Confirm that a node is spawned correctly
-handleSpawned(Node, Flag)->
-    io:format("Node ~p with id ~s spawned, initiator: ~p ~n", [self(), Node, Flag ]).
+
+handleSpawned(Node) ->
+    io:format("Node ~s with pid ~p spawned~n", [Node, self()]),
+    receive
+        Neighbours -> io:format("~p has neighbours ~p~n", [Node, Neighbours])
+    end.
+
