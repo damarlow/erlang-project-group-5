d46940802e451995f3dbd6543349da04f558ee62
Dave Marlow <dam486@cs.bham.ac.uk>
2018-03-18 01:11:55 +0000

    Have written function to retrieve the neighbour PIDs of nodes per node, so that they can have neighbours. Still to do: implement tarry

--- src/tarry.erl
+++ src/tarry.erl
@@ -1,6 +1,6 @@
 -module(tarry).
 
--export([main/0, testSpawned/2]).
+-export([main/0, handleSpawned/2, getNeighbours/2]).
 
 readInput(Lines) ->
   case io:get_line("") of
@@ -16,9 +16,7 @@ readInput(Lines) ->
 main() ->
     Out = readInput([]),
     % Spawn the nodes from the output
-    % _list will hold the ordered list of tarry
-    % remove leading _ before use
-    _list = spawnNodes(Out).
+    spawnNodes(Out).
     %    io:format("~p~n", [Out]).
 
 spawnNodes([Head | Tail])->
@@ -26,18 +24,24 @@ spawnNodes([Head | Tail])->
     % Use a list composition to get the name of each node (i.e. first elem of each sublist)
     NodeNames = [lists:sublist(Node, 1, 1) || Node <- Tail],
     % The neighbours of each are the rest of the elements of each sublist
-    % Remove leading _ before using, as that indicates it is unbound for -Werror
-    _Neighbours =  [dropFirst(SubList) || SubList <- Tail],
+    Neighbours = [lists:reverse(lists:droplast(lists:reverse(SubList)))|| SubList <- Tail],
     % This spawns nodes and keeps Pids
-    % _Pids needs to remove _ before using, as it is currently unbound
-    _Pids = [spawn(tarry, testSpawned, [Name, Name =:= Head]) || Name <- NodeNames].
+    Pids = [spawn(tarry, handleSpawned, [Name, Name =:= Head]) || Name <- NodeNames],
+    % Zip the Pids together with corresponding nodes
+    NodeIDs = lists:zip(NodeNames, Pids),
+    % Get a list of {PID, Neighbour-PID-list} tuples, giving a list of nodes that are neighbours for each node
+    NeighbourIDs = [getNeighbours(N, NodeIDs)|| N <- lists:zip(NodeIDs, Neighbours)],
+    % Still need to do something with this
+    % Possibly [Pid ! Neighbours || {Pid, Neighbours} <- NeighbourIDs]
+    % Need to work out how to handle that though
+    io:format("~p~n", [NeighbourIDs]).
 
-
+getNeighbours({{_, PID}, NeighboursForOne}, NodeIDs)->
+    % Function to get the PID for a node
+    FuncMap = fun(Node) -> element(2, lists:keyfind([Node], 1, NodeIDs)) end,
+    % Return a tuple of the PID and the PIDs of its neighbours
+    {PID, lists:map(FuncMap, NeighboursForOne)}.
+		
 % Confirm that a node is spawned correctly
-testSpawned(Node, Flag)->
-    io:format("Node ~p with id ~s, initiator: ~p ~n", [self(), Node, Flag ]).
-
-
-% To make code look nicer when dropping the first and keeping in order
-dropFirst(List)->
-    lists:reverse(lists:droplast(lists:reverse(List))).
+handleSpawned(Node, Flag)->
+    io:format("Node ~p with id ~s spawned, initiator: ~p ~n", [self(), Node, Flag ]).
